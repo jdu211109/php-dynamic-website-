05ca42dd35e89f88b2858571d45165adb61a0f09
jdu211109 <211109@jdu.uz>
2023-04-14 14:34:00 +0500

    php dynamic website

---
+++ about.php
@@ -0,0 +1,9 @@
+<?php
+include "header.html";
+?>
+
+<h1>about php</h1>
+
+<?php
+include "footer.html";
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include "header.html";
+?>
+
+<?php
+echo "Hello im php code";
+echo 8643534523623624653453453 * 732365686574563523;
+?>
+
+<?php
+include "footer.html";
+?>
